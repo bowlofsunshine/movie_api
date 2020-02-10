3f28da80a18b1c6d35667e71b718e533901fdcd0
Rachel <rachelgoree26@gmail.com>
2020-02-10 11:50:32 +0100

    added http, url, fs modules to return files  to the user and log request

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// imports the http model
+// imports the http, fs, url model
 const http = require('http'),
 fs = require('fs'),
 url = require('url');
@@ -10,18 +10,19 @@ http.createServer((request, response) => {
   q = url.parse(addr, true),
   filePath = '';
 
+  //Parse the request.url to determine if the URL contains the word “documentation”
   if (q.pathname.includes('documentation')) {
+    // If it does, return the “documentation.html” file to the user
     filePath = (_dirname + '/documentation.html');
   } else {
+    //otherwise return the “index.html” file
     filePath = "index.html";
   }
-
+  //For all requests coming in to your “server.js” file, use the fs module to log both the request URL and a timestamp to the “log.txt” file.
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp ' + new Date() + '\n\n', function(err) {
     if (err) {
       throw err;
-    } else {
-      console.log('added to log');
-    }
-  })
-  //listens for a response on port 8080    
+    } 
+  });
+  //listens for a response on port 8080
 }).listen(8080);
