5c940b31396f89da0b316fd955f1d856cd3be45f
Rachel <rachelgoree26@gmail.com>
2020-03-24 12:13:57 +0100

    Update main-view.jsx

--- client/components/main-view/main-view.jsx
+++ client/components/main-view/main-view.jsx
@@ -1,19 +1,81 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component {
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
     constructor() {
         // call the superclass constructor so react can initialize it 
         super();
 
         //initialize the state to an empty object so we can destructure it later
-        this.state = {};
+        this.state = {
+            movies: null,
+            selectedMovie: null
+        };
+    }
+    //ome of the hooks available in react component 
+    componentDidMount() {
+        axios.get('<myflixyappy1226.herokuapp.com/movies>')
+            .then(response => {
+                //assign the resilt to the state 
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    onMovieClick(movie) {
+        this.setState({
+            selectedMovie: movie
+        });
     }
 
     //this overrides the rendure() method of the superclass, no need to call super() though, as it does nothing by default 
     render() {
         return (
             <div className="main-view"></div>
+        );
+    }
+}
+
+
+export class MainView extends React.Component {
+    //ome of the hooks available in react component 
+    componentDidMount() {
+        axios.get('<myflixyappy1226.herokuapp.com/movies>')
+            .then(response => {
+                //assign the resilt to the state 
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        //if the state isnt initialized this will throw on runtime before the data is initally loaded 
+        const { movies, selectedMovie } = this.state;
+
+        //before the movie have been loaded 
+        if (!movies) return <div className="main-view" />;
+
+        return (
+            <div className="main-view">
+                {selectedMovie
+                    ? <MovieView movie={selectedMovie} />
+                    : movies.map(movie => (
+                        <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+                    ))
+                }
+
+            </div>
         );
     }
 }
