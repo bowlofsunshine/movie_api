73181334f287c5111aac73959f8268b287827f2f
Rachel <rachelgoree26@gmail.com>
2020-03-24 19:52:11 +0100

    added image path

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -30494,7 +30494,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50342" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57660" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
